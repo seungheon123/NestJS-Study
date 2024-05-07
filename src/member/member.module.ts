9af1262fc67ce4424baa64b66b6ec683217c100f
seungheon123 <seungheon.bisb.2012@gmail.com>
2024-05-07 15:18:56 +0900

    Create Member Repository Interface

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -3,11 +3,18 @@ import { MemberService } from './member.service';
 import { MemberController } from './member.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entities/member.entity';
-import { MemberRepository } from './member.repository';
+import { MemberRepositoryImpl } from './member.repositoryImpl';
+import { MEMBER_REPOSITORY } from './member.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Member])],
   controllers: [MemberController],
-  providers: [MemberService],
+  providers: [
+    MemberService,
+    {
+      provide: MEMBER_REPOSITORY,
+      useClass: MemberRepositoryImpl
+    }
+  ],
 })
 export class MemberModule {}
--- src/member/member.repository.ts
+++ src/member/member.repository.ts
@@ -1,24 +1,10 @@
-import { DataSource, Repository } from "typeorm";
+import { Injectable } from "@nestjs/common";
 import { Member } from "./entities/member.entity";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateMemberDto } from "./dto/create-member.dto";
 
-@Injectable()
-export class MemberRepository extends Repository<Member>{
-    constructor(private readonly datasource: DataSource){
-        
-        super(Member, datasource.createEntityManager());
-    }
+export const MEMBER_REPOSITORY = "Member Repository"
 
-    async getMemberById(id: number): Promise<Member>{
-        const queryRunner = this.datasource.createQueryRunner();
-        await queryRunner.connect();
-        await queryRunner.startTransaction();
-        const found = await this.findOneBy({'id':id});
-        if(!found){
-            throw new NotFoundException(`Can't find member with id" ${id}`);
-        }
-        return found;
-    }
-
-
+export interface MemberRepository{
+    create(createMemberDto: CreateMemberDto):Member ;
+    save(member: Member): Promise<Member>;
 }
